28e7052cbc87413b255ec48ff9202658253b310e
IrenZha <129683328+IrenZha@users.noreply.github.com>
2023-04-11 21:54:40 +0300

    sequence 7 14 21...

---
+++ src/main/java/ru/teachmeskills/homework3/TaskCycle4.java
@@ -0,0 +1,11 @@
+package ru.teachmeskills.homework3;
+
+public class TaskCycle4 {
+    public static void main(String[] args) {
+        int i=7;
+        while (i<100) {
+            System.out.print(i+" ");
+            i+=7;
+        }
+    }
+}
