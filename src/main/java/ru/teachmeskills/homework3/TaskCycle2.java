4290947424b8dc876548ed093677ea993eea3982
IrenZha <129683328+IrenZha@users.noreply.github.com>
2023-04-11 20:46:30 +0300

    numbers 5-1

---
+++ src/main/java/ru/teachmeskills/homework3/TaskCycle2.java
@@ -0,0 +1,10 @@
+package ru.teachmeskills.homework3;
+
+public class TaskCycle2 {
+    public static void main(String[] args) {
+        int i=5;
+        for (; i>0; i--){
+            System.out.println(i);
+        }
+    }
+}
