f6ea5becff1f228fbccb32408a84e0687d1e3c38
IrenZha <129683328+IrenZha@users.noreply.github.com>
2023-04-12 09:26:53 +0300

    percent

---
+++ src/main/java/ru/teachmeskills/homework3/Bank.java
@@ -0,0 +1,18 @@
+package ru.teachmeskills.homework3;
+
+import java.util.Scanner;
+
+public class Bank {
+    public static void main(String[] args) {
+        System.out.println("Введите сумму вклада");
+        Scanner scanner = new Scanner(System.in);
+        float sum = scanner.nextFloat();
+        System.out.println("Введите количество месяцев");
+        Scanner input = new Scanner(System.in);
+        int month = scanner.nextInt();
+        for (int i=month;i>0; i--){
+        sum += sum*7/100;
+        }
+        System.out.println("Сумма вклада с учетом процентов за "+month+" месяцев составит "+sum);
+    }
+}
