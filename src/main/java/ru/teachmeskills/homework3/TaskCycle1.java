4a41220912d43b66a63affb065f02a4bcb097b5f
IrenZha <129683328+IrenZha@users.noreply.github.com>
2023-04-14 17:14:19 +0300

    correct

--- src/main/java/ru/teachmeskills/homework3/TaskCycle1.java
+++ src/main/java/ru/teachmeskills/homework3/TaskCycle1.java
@@ -2,8 +2,8 @@ package ru.teachmeskills.homework3;
 
 public class TaskCycle1 {
     public static void main(String[] args) {
-for (int i=1; i<100; i+=2){
-    System.out.println(i);
-}
+        for (int i = 1; i < 100; i += 2) {
+            System.out.println(i);
+        }
     }
 }
