3e2aa5ed378c9dcfd2d200bf9ff05e4ff3f7699f
IrenZha <129683328+IrenZha@users.noreply.github.com>
2023-04-14 17:20:22 +0300

    correct

--- src/main/java/ru/teachmeskills/homework3/TaskCycle3.java
+++ src/main/java/ru/teachmeskills/homework3/TaskCycle3.java
@@ -7,9 +7,9 @@ public class TaskCycle3 {
         System.out.println("Введите целое положительное число");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        int summ=0;
-        for (int i=1; i<number; i++) summ += i;
-        System.out.println("Сумма чисел от 1 до "+ number + " равна " + summ);
+        int summ = 0;
+        for (int i = 1; i < number; i++) summ += i;
+        System.out.println("Сумма чисел от 1 до " + number + " равна " + summ);
     }
 
 }
