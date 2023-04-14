5f6990bc7ca4929b607d3cd8718b9ee12fe50ba0
IrenZha <129683328+IrenZha@users.noreply.github.com>
2023-04-14 18:13:02 +0300

    number in array

---
+++ src/main/java/ru/teachmeskills/homework4/HW4_0.java
@@ -0,0 +1,28 @@
+package ru.teachmeskills.homework4;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class HW4_0 {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        System.out.println("Введите число от 0 до 10");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Random random = new Random();
+        boolean test = false;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10);
+        }
+        System.out.println(Arrays.toString(array));
+        for (int i : array) {
+            if (i == n) {
+                test = true;
+                System.out.println("Заданное число входит в массив");
+                break;
+            }
+        }
+        if (!test) System.out.println("Заданное число не входит в массив");
+    }
+}
