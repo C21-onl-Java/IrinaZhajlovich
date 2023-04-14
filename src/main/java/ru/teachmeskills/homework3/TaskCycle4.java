16b458a5742ddbcd6b1dfb7e95bc7699a50e1727
IrenZha <129683328+IrenZha@users.noreply.github.com>
2023-04-14 17:21:30 +0300

    correct

--- src/main/java/ru/teachmeskills/homework3/TaskCycle4.java
+++ src/main/java/ru/teachmeskills/homework3/TaskCycle4.java
@@ -2,10 +2,10 @@ package ru.teachmeskills.homework3;
 
 public class TaskCycle4 {
     public static void main(String[] args) {
-        int i=7;
-        while (i<100) {
-            System.out.print(i+" ");
-            i+=7;
+        int i = 7;
+        while (i < 100) {
+            System.out.print(i + " ");
+            i += 7;
         }
     }
 }
