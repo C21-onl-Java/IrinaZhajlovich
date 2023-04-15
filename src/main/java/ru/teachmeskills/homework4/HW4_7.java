00c11e332d5a8764ae7071a1b948c26536c4ac3e
IrenZha <129683328+IrenZha@users.noreply.github.com>
2023-04-15 20:18:41 +0300

    bubbleSort

---
+++ src/main/java/ru/teachmeskills/homework4/HW4_7.java
@@ -0,0 +1,23 @@
+package ru.teachmeskills.homework4;
+
+import java.util.Arrays;
+
+public class HW4_7 {
+    public static void main(String[] args) {
+        int[] sortArr = {12, 6, 4, 1, 15, 10};
+        System.out.println(Arrays.toString(sortArr));
+        int toSwap;
+        for (int i = sortArr.length - 1; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (sortArr[j] > sortArr[j + 1]) {
+                    toSwap = sortArr[j];
+                    sortArr[j] = sortArr[j + 1];
+                    sortArr[j + 1] = toSwap;
+                }
+            }
+        }
+        System.out.print(Arrays.toString(sortArr));
+    }
+}
+
+
