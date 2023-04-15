650d14d6c3ea2bd3c6997f9985c7f4cfc75d537f
IrenZha <129683328+IrenZha@users.noreply.github.com>
2023-04-15 20:24:19 +0300

    sort array

--- src/main/java/ru/teachmeskills/homework4/HW4_6.java
+++ src/main/java/ru/teachmeskills/homework4/HW4_6.java
@@ -6,11 +6,10 @@ public class HW4_6 {
     public static void main(String[] args) {
         String[] arr = {"Victor", "Marina", "Anna", "Jack"};
         System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.binarySearch(arr,"Anna"));
+        System.out.println(Arrays.binarySearch(arr, "Anna"));
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.binarySearch(arr,"Anna"));
-        System.out.print(Arrays.binarySearch(arr,"Victor"));
+        System.out.println(Arrays.binarySearch(arr, "Anna"));
     }
 }
 
