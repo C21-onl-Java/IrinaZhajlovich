e75e184a2f5c8d02970880bfb8f510fdff451060
37544 <brinrf@mail.ru>
2023-05-27 14:18:20 +0300

    Task 2

---
+++ src/main/java/ru/teachmeskills/homework11/task2/Main.java
@@ -0,0 +1,8 @@
+package ru.teachmeskills.homework11.task2;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(User.confirmUser("egrtt kjdlvj", "123qw", "123qw"));
+
+    }
+}
