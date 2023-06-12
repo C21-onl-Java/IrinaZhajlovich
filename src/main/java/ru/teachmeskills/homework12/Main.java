db8a92d7896248ed647c131460a0c99a11a09ff5
37544 <brinrf@mail.ru>
2023-06-12 11:52:32 +0300

    Task 2

--- src/main/java/ru/teachmeskills/homework12/Main.java
+++ src/main/java/ru/teachmeskills/homework12/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //    C:\Users\37544\IdeaProjects\IrinaZhajlovich\src\main\java\ru\teachmeskills\homework12\Numbers
+        //  src\main\java\ru\teachmeskills\homework12\Numbers
         FileProcessing.isNumberValid();
 
     }
